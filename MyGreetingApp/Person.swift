1aa4515ef34bdb91c13abc7e847c50fbe4dc2f3c
Denis Gindulin <denisgindulin@me.com>
2024-02-17 19:06:06 +0300

    MVC example

---
+++ MyGreetingApp/Person.swift
@@ -0,0 +1,11 @@
+//
+//  Person.swift
+//  MyGreetingApp
+//
+//  Created by Денис Гиндулин on 17.02.2024.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+}
--- MyGreetingApp/ViewController.swift
+++ MyGreetingApp/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
+// в архитектурном паттерне MVC (Model-View-Controller) распределение модулей хромает: View и Controller очень неразрывно связаны друг с другом и роли между ними не разделены, тестируемость затруднительна, но код получился самым коротким
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    
+    // объявляю свойство person с типом Person. Неявно извлекаю опционал, поставив восклицательный знак, тем самым указываю, что свойство надо обязательно инициализировать (подготовить, наполнив значением). Сделаю инициализацию в методе viewDidLoad.
+    private var person: Person!
+    
+    // переопределяю метод жизненного цикла ViewController-а viewDidLoad для того, чтобы инициализировать в нем свойство person в качестве экземпляра модели Person
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // в рамках инициализации свойства person присваиваю значения свойствам модели Person
+        person = Person(name: "Tim", surname: "Cook")
     }
-
-
+    
+    // переопределяю метод touchesBegan, который отвечает за реакцию на касание пользоватедем экрана в любой области, чтобы в связи с касанием изменить текст в лейбле greetingLabel
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        // присваиваю тексту лейбла значение "Hello, Tim Cook!", которое будет появляться в ответ на касание пользователя по экрану
+        greetingLabel.text = "Hello, \(person.name) \(person.surname)"
+    }
 }
 
