04819c4212e704af395b561d9a64b3e740509f7a
Denis Gindulin <denisgindulin@me.com>
2024-02-17 19:24:34 +0300

    add some comments

--- MyGreetingApp/Person.swift
+++ MyGreetingApp/Person.swift
@@ -5,6 +5,7 @@
 //  Created by Денис Гиндулин on 17.02.2024.
 //
 
+// это Model в рамках архитектурного паттерна MVC (Model-View-Controller)
 struct Person {
     let name: String
     let surname: String
