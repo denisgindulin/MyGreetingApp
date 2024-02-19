61c045d299337a09e22ffa34e11ab3ffaeb10844
Denis Gindulin <denisgindulin@me.com>
2024-02-19 19:33:51 +0300

    added some comments

--- MyGreetingApp/GreetingViewController.swift
+++ MyGreetingApp/GreetingViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-// подписываю сам протокол GreetingViewProtocol под протокол AnyObject, чтобы он стал протоколом ссылочного типа и пропала ошибка в классе GreetingPresenter при объявлении свойства view с указателем unowned
+// создал протокол, который будет публичным интерфейсом для класса GreetingViewController, то есть для архитектурного модуля View из архитектурного паттерна MVP (Model - View - Presenter). В MVP данные между архитектурными модулями передаются посредством протоколов. Этот протокол обязывает класс GreetingViewController реализовать метод setGreeting. Реализацию метода делаю ниже в расширении в этом файле (см. extension GreetingViewController). Суть реализации метода будет в том, чтобы в свойство text элемента интерфейса greetingLabel класса UILabel установить значение, которое будет передано в параметр greeting этого метода. Имя протокола сложил с трех компонентов: 1. имя сцены (Greeting); 2. имя архитектурного модуля (или класса, который будет подписываться под протокол) (View); 3. слово Protocol. Делегирующим классом для класса GreetingViewController, то есть классом, где будет подготовлено значение для параметра greeting метода setGreeting этого протокола, станет класс GreetingPresenter, представляющий собой архитектурный модуль Presenter. Классы GreetingViewController и GreetingPresenter будут ссылаться друг на друга, поэтому между ними будет образовываться цикл сильных ссылок, который надо будет разорвать, сделав одну из ссылок unowned (см. файл GreetingPresenter). На появление unowned-ссылки вызовет в Xcode предупреждение об ошибке (красного ворнинга): 'unowned' must not be applied to non-class-bound 'any GreetingViewProtocol'; consider adding a protocol conformance that has a class bound. Поэтому подписываю сам протокол GreetingViewProtocol под протокол AnyObject, чтобы он стал протоколом ссылочного типа (reference type) и пропала ошибка в классе GreetingPresenter при объявлении свойства view с указателем unowned
 protocol GreetingViewProtocol: AnyObject {
+    // это метод протокола, поэтому здесь только имя метода и параметр с указанием типа в круглых скобках, а реализации метода нет (нет фигурных скобок). Реализация будет в классе, который подпишется под этот протокол.
     func setGreeting(_ greeting: String)
 }
 
+// это класс, представляющий собой архитектурный модуль View в архитектурном паттерне MVP (Model - View - Presenter). В MVP модуль View соединяет в себе View и Сontroller, поэтому имя класса (и соответствующего ему файла) сложил из двух компонентов: 1. имя сцены (Greeting); 2. слово ViewController. Класс GreetingViewController унаследовал от суперкласса UIViewController из объектной библиотеки UIKit
 class GreetingViewController: UIViewController {
-
+    // 
     @IBOutlet var greetingLabel: UILabel!
     
     private var presenter: GreetingPresenterProtocol!
--- MyGreetingApp/Person.swift
+++ MyGreetingApp/Person.swift
@@ -5,7 +5,7 @@
 //  Created by Денис Гиндулин on 17.02.2024.
 //
 
-// это модуль Model в архитектурном паттерне MVP (Model-View-Presenter)
+// это архитектурный модуль Model в архитектурном паттерне MVP (Model-View-Presenter) - структура, представляющая собой модель данных Person, у которой есть свойства name и surname
 struct Person {
     let name: String
     let surname: String
