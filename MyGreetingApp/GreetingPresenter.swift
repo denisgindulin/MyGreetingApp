6b48675a8eb8eded14d0e91366a7967b0e8d021a
Denis Gindulin <denisgindulin@me.com>
2024-02-19 21:11:41 +0300

    added some comments again

--- MyGreetingApp/GreetingPresenter.swift
+++ MyGreetingApp/GreetingPresenter.swift
@@ -5,23 +5,30 @@
 //  Created by Денис Гиндулин on 17.02.2024.
 //
 
-// 
+// создал протокол, который будет публичным интерфейсом для класса GreetingPresenter, то есть для архитектурного модуля Presenter из архитектурного паттерна MVP (Model - View - Presenter). В MVP данные между архитектурными модулями передаются посредством протоколов. Этот протокол обязывает класс GreetingPresenter реализовать обязательный инициализатор (со свойствами view и person) и метод showGreeting. Реализацию метода делаю ниже в самом классе GreetingPresenter. Суть реализации метода будет в том, чтобы создать объект приветствия (фразу "Hello, ...!") и передать его в модуль View. Также у модуля Presenter будет связь с модулем Model. Имя протокола сложил из трех компонентов: 1. имя сцены (Greeting); 2. имя архитектурного модуля (или класса, который будет подписываться под протокол) (Presenter); 3. слово Protocol. Делегирующим классом для класса GreetingPresenter станет класс GreetingViewController, представляющий собой архитектурный модуль View. Классы GreetingViewController и GreetingPresenter будут ссылаться друг на друга, поэтому между ними будет образовываться цикл сильных ссылок, который надо будет разорвать, сделав одну из ссылок unowned (см. файл GreetingPresenter). На появление unowned-ссылки вызовет в Xcode предупреждение об ошибке (красного ворнинга): 'unowned' must not be applied to non-class-bound 'any GreetingViewProtocol'; consider adding a protocol conformance that has a class bound. Поэтому подписываю сам протокол GreetingViewProtocol под протокол AnyObject, чтобы он стал протоколом ссылочного типа (reference type) и пропала ошибка в классе GreetingPresenter при объявлении свойства view с указателем unowned.
 protocol GreetingPresenterProtocol {
+    //
     init(view: GreetingViewProtocol, person: Person)
     func showGreeting()
 }
 
 class GreetingPresenter: GreetingPresenterProtocol {
+    // объявляю в классе GreetingPresenter свойство view с типом GreetingPresenterProtocol (тем самым создал связь между архитектурными модулями View и Presenter). Использовал ключевое слово private, чтобы ограничить доступ к свойству view откуда-либо вне пределов класса GreetingPresenter. Использовал ключевое слово unowned, чтобы разорвать цикл сильных ссылок, который мог бы образоваться из-за взаимных ссылок друг н друга классов GreetingPresenter и GreetingViewController. Чтобы указать, что ссылка не является сильной, использовал ключевое слово unowned, а не weak, так как свойство view не имеет опциональный тип. Из двух классов (GreetingPresenter и GreetingViewController) родительским классом будет GreetingViewController, дочерним - GreetingPresenter, так как GreetingViewController порождается первым. Из-за этого ключевое слово unowned использовал в классе GreetingPresenter (такое правило для разрыва цикла сильных ссылок).
     private unowned let view: GreetingViewProtocol
+    // объявляю в классе GreetingPresenter свойство person с типом Person, то есть создал экземпляр модели в презентере. Использовал ключевое слово private, чтобы ограничить доступ к свойству view откуда-либо вне пределов класса GreetingPresenter. Свойства view и person необходимы для реализации обязательного инициализатора строкой ниже, которую требовал протокол GreetingPresenterProtocol.
     private let person: Person
     
+    // это обязательный инициализатор. С помощью него также создается связь модуля Presenter с модулями View и Model. В результате свойство view класса GreetingPresenter получит в качестве значения view из класса GreetingViewController, а свойство person класса GreetingPresenter получит в качестве значения person из структуры Person. Это необходимо, чтобы позже в этом классе реализовать метод showGreeting.
     required init(view: GreetingViewProtocol, person: Person) {
         self.view = view
         self.person = person
     }
     
+    // это реализация метода showGreeting. Суть: создать объект приветствия (фразу "Hello, ...!") и передать его в модуль View. Реализовать его надо, исходя из требований протокола GreetingPresenterProtocol. Вызывать этот метод буду в классе GreetingViewController в рамках переопределения метода touchesBegan, который срабатывает при касании пользователем экрана устройства. То есть модуль View не готовит приветственную фразу, этим занимается Presenter.
     func showGreeting() {
+        // создаю объект приветствия. Данные для него модуль Presenter берет из модуля Model, обращаясь к свойствам name и surname экземпляра структуры Person.
         let greeting = "Hello, \(person.name) \(person.surname)!"
+        // обращаюсь к свойству view этого класса. У view тип протокола GreetingViewProtocol, который размещен в файле GreetingViewController и содержит метод setGreeting. Поэтому, если точнее, обращаюсь к методу setGreeting свойства view класса GreetingPresenter. У метода setGreeting есть параметр greeting. Передаю в него в качестве значения объект greeting (фразу приветствия), который создал в предыдущей строке.
         view.setGreeting(greeting)
     }
     
--- MyGreetingApp/GreetingViewController.swift
+++ MyGreetingApp/GreetingViewController.swift
@@ -5,9 +5,10 @@
 //  Created by Денис Гиндулин on 16.02.2024.
 //
 
+// импортировал нативный фреймворк UIKit, чтобы в этом файле использовать классы из объектной библиотеки UIKit - UIViewController, UILabel
 import UIKit
 
-// создал протокол, который будет публичным интерфейсом для класса GreetingViewController, то есть для архитектурного модуля View из архитектурного паттерна MVP (Model - View - Presenter). В MVP данные между архитектурными модулями передаются посредством протоколов. Этот протокол обязывает класс GreetingViewController реализовать метод setGreeting. Реализацию метода делаю ниже в расширении в этом файле (см. extension GreetingViewController). Суть реализации метода будет в том, чтобы в свойство text элемента интерфейса greetingLabel класса UILabel установить значение, которое будет передано в параметр greeting этого метода. Имя протокола сложил с трех компонентов: 1. имя сцены (Greeting); 2. имя архитектурного модуля (или класса, который будет подписываться под протокол) (View); 3. слово Protocol. Делегирующим классом для класса GreetingViewController, то есть классом, где будет подготовлено значение для параметра greeting метода setGreeting этого протокола, станет класс GreetingPresenter, представляющий собой архитектурный модуль Presenter. Классы GreetingViewController и GreetingPresenter будут ссылаться друг на друга, поэтому между ними будет образовываться цикл сильных ссылок, который надо будет разорвать, сделав одну из ссылок unowned (см. файл GreetingPresenter). На появление unowned-ссылки вызовет в Xcode предупреждение об ошибке (красного ворнинга): 'unowned' must not be applied to non-class-bound 'any GreetingViewProtocol'; consider adding a protocol conformance that has a class bound. Поэтому подписываю сам протокол GreetingViewProtocol под протокол AnyObject, чтобы он стал протоколом ссылочного типа (reference type) и пропала ошибка в классе GreetingPresenter при объявлении свойства view с указателем unowned.
+// создал протокол, который будет публичным интерфейсом для класса GreetingViewController, то есть для архитектурного модуля View из архитектурного паттерна MVP (Model - View - Presenter). В MVP данные между архитектурными модулями передаются посредством протоколов. Этот протокол обязывает класс GreetingViewController реализовать метод setGreeting. Реализацию метода делаю ниже в расширении в этом файле (см. extension GreetingViewController). Суть реализации метода будет в том, чтобы в свойство text элемента интерфейса greetingLabel класса UILabel установить значение, которое будет передано в параметр greeting этого метода. Имя протокола сложил из трех компонентов: 1. имя сцены (Greeting); 2. имя архитектурного модуля (или класса, который будет подписываться под протокол) (View); 3. слово Protocol. Делегирующим классом для класса GreetingViewController, то есть классом, где будет подготовлено значение для параметра greeting метода setGreeting этого протокола, станет класс GreetingPresenter, представляющий собой архитектурный модуль Presenter. Классы GreetingViewController и GreetingPresenter будут ссылаться друг на друга, поэтому между ними будет образовываться цикл сильных ссылок, который надо будет разорвать, сделав одну из ссылок unowned (см. файл GreetingPresenter). На появление unowned-ссылки вызовет в Xcode предупреждение об ошибке (красного ворнинга): 'unowned' must not be applied to non-class-bound 'any GreetingViewProtocol'; consider adding a protocol conformance that has a class bound. Поэтому подписываю сам протокол GreetingViewProtocol под протокол AnyObject, чтобы он стал протоколом ссылочного типа (reference type) и пропала ошибка в классе GreetingPresenter при объявлении свойства view с указателем unowned.
 protocol GreetingViewProtocol: AnyObject {
     // это метод протокола, поэтому здесь только имя метода и параметр с указанием типа в круглых скобках, а реализации метода нет (нет фигурных скобок). Реализация будет в классе, который подпишется под этот протокол.
     func setGreeting(_ greeting: String)
@@ -18,6 +19,7 @@ class GreetingViewController: UIViewController {
     // это связь между вью и контроллером. Здесь объявил свойство greetingLabel с типом UILabel (класс из объектной библиотеки UIKit). Неявно извлекаю опционал, поставив восклицательный знак, тем самым указываю, что свойство требует обязательной инициализации (подготовить, наполнив значением). Инициализацию - присвоение стартового значения - сделал в сториборде в файле Main (а именно: свойству text класса UILabel присвоил значение "Please tap to Screen")
     @IBOutlet var greetingLabel: UILabel!
     
+    //объявляю в классе GreetingViewController свойство presenter с типом GreetingPresenterProtocol (то есть создал экземпляр модели во вью контроллере, тем самым создал связь между архитектурными модулями). Неявно извлекаю опционал, поставив восклицательный знак, тем самым указываю, что свойство надо обязательно инициализировать (подготовить, наполнив значением). Позже сделаю инициализацию в методе viewDidLoad. Использовал ключевое слово private, чтобы ограничить доступ к свойству person откуда-либо вне пределов класса GreetingViewController. Использовал ключевое слово var, так как свойство person имеет опциональный тип.
     private var presenter: GreetingPresenterProtocol!
     
     override func viewDidLoad() {
@@ -37,3 +39,25 @@ extension GreetingViewController: GreetingViewProtocol {
         greetingLabel.text = greeting
     }
 }
+
+/*
+ // объявляю свойство person с типом Person (то есть создал экземпляр модели во вью контроллере, тем самым создал связь между архитектурными модулями). Неявно извлекаю опционал, поставив восклицательный знак, тем самым указываю, что свойство надо обязательно инициализировать (подготовить, наполнив значением). Позже сделаю инициализацию в методе viewDidLoad. Использовал ключевое слово private, чтобы ограничить доступ к свойству person откуда-либо вне пределов класса GreetingViewController. Использовал ключевое слово var, так как свойство person имеет опциональный тип.
+ private var person: Person!
+ 
+ // переопределяю метод жизненного цикла вью контроллера viewDidLoad для того, чтобы инициализировать в нем свойство person в качестве экземпляра модели Person
+ override func viewDidLoad() {
+     // поскольку нахожусь в переопределяемом методе, то сначала обращаюсь к суперклассу
+     super.viewDidLoad()
+     // в рамках инициализации свойства person присваиваю значения свойствам модели Person
+     person = Person(name: "Tim", surname: "Cook")
+ }
+ 
+ // переопределяю метод touchesBegan, который отвечает за реакцию на касание пользователем экрана в любой области, чтобы в связи с касанием изменить текст в лейбле greetingLabel
+ override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     // поскольку нахожусь в переопределяемом методе, то сначала обращаюсь к суперклассу
+     super.touchesBegan(touches, with: event)
+     // присваиваю тексту лейбла значение "Hello, Tim Cook!", которое будет появляться в ответ на касание пользователя по экрану. Для этого использую интерполяцию (символы \()), добавляя в строку значения свойств name и surname экземпляра person модели Person
+     greetingLabel.text = "Hello, \(person.name) \(person.surname)"
+ }
+}
+ */
